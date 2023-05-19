0975694e98f69506250a330ef75db670692cfffe
unknown <michaelmolina679@gmail.com>
2023-05-19 16:40:45 -0600

    agregando custom hooks y isLoading

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,27 +1,19 @@
-import { useEffect, useState } from "react";
 import PropTypes from 'prop-types';
-import { getGifs } from "../helpers/getGifs";
 import GifItem from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({category}) => {
 
-    const [images, setImages] = useState([]);
-
-    const getImages = async() => {
-        const newImages = await getGifs(category);
-        setImages(newImages);
-    }
-
-    useEffect(()=> {
-        getImages();
-    }, []);
-
+    const { images, isLoading } = useFetchGifs(category);
+    // console.log(isLoading);
+    
     return (
         <>
             <h4>{category}</h4>
+            { isLoading &&  <h2>Cargando...</h2> }
             <ol className="card-grid">
                 {images.map((image) => (
-                    // <GifItem key={id} title={title} url={url} /> //Forma tradicional de desestructurar
+                // <GifItem key={id} title={title} url={url} /> //Forma tradicional de desestructurar
                     <GifItem key={image.id} {...image} /> //Esparcir las propiedades en caso de tener muchas
                 ))}
             </ol>
